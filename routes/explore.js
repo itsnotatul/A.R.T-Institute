869732c7df7f3127fa4f36fdb53562b07af85570
root <atultiwariee60@gmail.com>
2020-04-15 22:38:26 +0000

    added the explore route and its rendering page

---
+++ routes/explore.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router  = express.Router();
+var passport= require("passport");
+var User    = require("../models/user");
+var mongoose = require("mongoose");
+
+router.get("/explore",function(req,res){
+	res.render("explore");
+});
